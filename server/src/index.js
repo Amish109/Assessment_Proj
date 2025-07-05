3f8fe8b3ad373bc7e03b3023752a9afc5d679e53
Amish109 <tiwariamish786@gmail.com>
2025-07-05 16:27:17 +0530

    changes for deployment

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,27 @@
-// import connectDb from "./database_config/index.js";
-import app from "./app.js";
-import { DOTENV_VARIABLES } from "./constants/dotenv_variables.js";
-// await connectDb();
-
+import express from 'express';
+import cors from 'cors';
+import router from './router/api_route.js';
+import { DOTENV_VARIABLES } from './constants/dotenv_variables.js';
 const {PORT} = DOTENV_VARIABLES;
+
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use('/api', router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+export default app;
